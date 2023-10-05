f4afc1c609aebcdb4fd8b528ea892ae17153cbfa
VValdo <v.sersam@gmail.com>
2023-10-05 11:03:23 +0200

    composition init

--- User.cs
+++ User.cs
@@ -2,23 +2,5 @@
 
 public class User
 {
-    public string FirstName;
-    public string LastName;
-    public readonly string Username;
-    public string Email;
-    public string DOB;
-    private string _password;
-
-    // Constructor till Readonly
-    // Ny måste alla klasser ha en konstruktor
-    public User(string username, string email)
-    {
-        Username = username;
-        Email = email;
-    }
-
-    public int Age()
-    {
-        throw new NotImplementedException();
-    }
+    public string Name;
 }
